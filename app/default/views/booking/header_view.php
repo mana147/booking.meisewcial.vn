80d61e0d0cab55baa1f38db9d435e10f3f3cb2fc
hieupt <hieuphamtrung@admicro.vn>
2022-07-06 13:08:40 +0700

    update

--- app/default/views/booking/header_view.php
+++ app/default/views/booking/header_view.php
@@ -16,6 +16,7 @@
 	<meta name="description" content="Booking" />
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
 
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
@@ -28,7 +29,6 @@
 	</script>
 
 	<!-- style -->
-
 	<style>
 		html,
 		body,
