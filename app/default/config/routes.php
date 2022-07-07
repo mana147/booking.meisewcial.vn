6e21566ee5cd51d1c03fe565642fca1505e73d1f
hieupt <hieuphamtrung@admicro.vn>
2022-07-07 16:39:38 +0700

    update

--- app/default/config/routes.php
+++ app/default/config/routes.php
@@ -49,7 +49,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'dashboard/dashboard/index';
+// $route['default_controller'] = 'dashboard/dashboard/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
