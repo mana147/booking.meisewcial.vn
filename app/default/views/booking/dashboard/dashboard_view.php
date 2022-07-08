422495806829263488186f5af285272d30b8a37c
hieupt <hieuphamtrung@admicro.vn>
2022-07-08 11:26:06 +0700

    update

--- app/default/views/booking/dashboard/dashboard_view.php
+++ app/default/views/booking/dashboard/dashboard_view.php
@@ -14,7 +14,7 @@
     <title>SB Admin 2 - Dashboard</title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?php echo base_url()?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo base_url()?>template/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
@@ -695,21 +695,21 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?php echo base_url()?>vendor/jquery/jquery.min.js"></script>
-    <script src="<?php echo base_url()?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php echo base_url()?>template/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo base_url()?>template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?php echo base_url()?>vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="<?php echo base_url()?>template/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="<?php echo base_url()?>js/sb-admin-2.min.js"></script>
+    <script src="<?php echo base_url()?>template/js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="<?php echo base_url()?>vendor/chart.js/Chart.min.js"></script>
+    <script src="<?php echo base_url()?>template/vendor/chart.js/Chart.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="<?php echo base_url()?>js/demo/chart-area-demo.js"></script>
-    <script src="<?php echo base_url()?>js/demo/chart-pie-demo.js"></script>
+    <script src="<?php echo base_url()?>template/js/demo/chart-area-demo.js"></script>
+    <script src="<?php echo base_url()?>template/js/demo/chart-pie-demo.js"></script>
 
 </body>
 
