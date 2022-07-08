06e0517e4dc8388f82f49dec381d252a9317978a
hieupt <hieuphamtrung@admicro.vn>
2022-07-08 18:39:45 +0700

    update

--- app/default/views/booking/landing/landing_view.php
+++ app/default/views/booking/landing/landing_view.php
@@ -46,7 +46,7 @@
             <a class="btn btn-primary btn-xl text-uppercase" href="#services">Tell Me More</a>
         </div>
     </header>
-    
+
     <!-- About-->
     <section class="page-section" id="about">
         <div class="container">
@@ -120,10 +120,12 @@
     <!-- Team-->
     <section class="page-section bg-light" id="team">
         <div class="container">
+
             <div class="text-center">
-                <h2 class="section-heading text-uppercase">Our Amazing Team</h2>
-                <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                <h2 class="section-heading text-uppercase">About Mei Sewcial House </h2>
+                <h3 class="section-subheading text-muted">ann.interior@gmail.com</h3>
             </div>
+
             <div class="row">
                 <div class="col-lg-4">
                     <div class="team-member">
@@ -156,11 +158,13 @@
                     </div>
                 </div>
             </div>
+
             <div class="row">
                 <div class="col-lg-8 mx-auto text-center">
                     <p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
                 </div>
             </div>
+
         </div>
     </section>
 
