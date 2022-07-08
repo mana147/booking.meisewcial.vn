737e696eaf3c99cdbab76b305ff6802d87c0e924
hieupt <hieuphamtrung@admicro.vn>
2022-07-08 11:28:12 +0700

    update

--- app/default/views/booking/dashboard/dashboard_view.php
+++ app/default/views/booking/dashboard/dashboard_view.php
@@ -695,21 +695,21 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="<?php echo base_url()?>template/vendor/jquery/jquery.min.js"></script>
-    <script src="<?php echo base_url()?>template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="template/vendor/jquery/jquery.min.js"></script>
+    <script src="template/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
-    <script src="<?php echo base_url()?>template/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="template/vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="<?php echo base_url()?>template/js/sb-admin-2.min.js"></script>
+    <script src="template/js/sb-admin-2.min.js"></script>
 
     <!-- Page level plugins -->
-    <script src="<?php echo base_url()?>template/vendor/chart.js/Chart.min.js"></script>
+    <script src="template/vendor/chart.js/Chart.min.js"></script>
 
     <!-- Page level custom scripts -->
-    <script src="<?php echo base_url()?>template/js/demo/chart-area-demo.js"></script>
-    <script src="<?php echo base_url()?>template/js/demo/chart-pie-demo.js"></script>
+    <script src="template/js/demo/chart-area-demo.js"></script>
+    <script src="template/js/demo/chart-pie-demo.js"></script>
 
 </body>
 
