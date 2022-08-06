8a606868a2778c8e6c9aa69f6c520fb87f6c76bc
hieupt <hieuphamtrung@admicro.vn>
2022-08-06 15:31:47 +0700

    update

--- app/default/views/booking/dashboard/dashboard_view.php
+++ app/default/views/booking/dashboard/dashboard_view.php
@@ -10,10 +10,10 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
+                <!-- <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">Booking Admin <sup>v0.1</sup></div>
+                </div> -->
+                <div class="sidebar-brand-text mx-3">Booking Admin<sup>v0.1</sup></div>
             </a>
 
             <!-- Divider -->
