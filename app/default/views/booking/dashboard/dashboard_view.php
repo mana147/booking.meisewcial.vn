5aceef22b0831701f4fbab99a890540c649246f2
hieupt <hieuphamtrung@admicro.vn>
2022-08-06 15:40:45 +0700

    update

--- app/default/views/booking/dashboard/dashboard_view.php
+++ app/default/views/booking/dashboard/dashboard_view.php
@@ -157,11 +157,10 @@
                     <ul class="navbar-nav ml-auto">
 
                         <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <li style="display: none;"  class="nav-item dropdown no-arrow d-sm-none">
+                        <!-- <li style="display: none;"  class="nav-item dropdown no-arrow d-sm-none">
                             <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-search fa-fw"></i>
                             </a>
-                            <!-- Dropdown - Messages -->
                             <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
                                 <form class="form-inline mr-auto w-100 navbar-search">
                                     <div class="input-group">
@@ -174,7 +173,7 @@
                                     </div>
                                 </form>
                             </div>
-                        </li>
+                        </li> -->
 
                         <!-- Nav Item - Alerts -->
                         <li style="display: none;"  class="nav-item dropdown no-arrow mx-1">
