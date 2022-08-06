30b03ad41f137ed91b127149c16156ed6ca6301a
hieupt <hieuphamtrung@admicro.vn>
2022-08-06 15:31:20 +0700

    update

--- app/default/views/booking/dashboard/dashboard_view.php
+++ app/default/views/booking/dashboard/dashboard_view.php
@@ -13,7 +13,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                <div class="sidebar-brand-text mx-3">Booking Admin <sup>v0.1</sup></div>
             </a>
 
             <!-- Divider -->
