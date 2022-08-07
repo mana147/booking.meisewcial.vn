3d6b3b67c1333b68185e38217aa41a705ff22aed
hieupt <hieuphamtrung@admicro.vn>
2022-08-07 19:31:34 +0700

    update

--- app/default/models/user/User_models.php
+++ app/default/models/user/User_models.php
@@ -21,44 +21,19 @@ class User_models extends CI_Model
 
         $query = $this->db->get();
 
-        foreach ($query->result() as $row) {
-            showLOG($row);
+        if ($query) {
+          
+            $info =  $query->result_array();
+
+            echo 'has user';
+            showLOG($info);
+            
+        } else {
+
+            echo 'not user';
         }
-
-        echo 'Total Results: ' . $query->num_rows();
-
-
+        
         die;
 
-        // $servername = "localhost";
-        // $username = "nugfhltmhosting_meisewcial";
-        // $password = "B?37XHO-_9]^";
-        // $dbname = "nugfhltmhosting_meisewcial";
-
-        // // Create connection
-        // $conn = new mysqli($servername, $username, $password, $dbname);
-        // // Check connection
-        // if ($conn->connect_error) {
-        //     die("Connection failed: " . $conn->connect_error);
-        // }
-
-        // $sql = "SELECT * FROM `wp_users`";
-
-        // $result = $conn->query($sql);
-
-        // if ($result->num_rows > 0) {
-        //     // output data of each row
-        //     while ($row = $result->fetch_assoc()) {
-        //         // echo "row: " . $row;
-
-        //         var_dump( $row);
-
-        //     }
-
-        // } else {
-        //     echo "0 results";
-        // }
-
-        // $conn->close();
     }
 }
