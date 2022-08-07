ae94b0e3ca4475ddf058f7c7c215e97fcb67ad2e
hieupt <hieuphamtrung@admicro.vn>
2022-08-07 19:39:28 +0700

    update

--- app/default/models/user/User_models.php
+++ app/default/models/user/User_models.php
@@ -25,15 +25,20 @@ class User_models extends CI_Model
 
             $info = $query->result_array();
 
-            if ($info && ( $user_password == $info['user_pass'] ) ) {
-                echo 'has user';
-                showLOG($info);
+            if ($info) {
+
+                if ($user_password == $info[0]['user_pass']) {
+
+                    echo 'has user';
+                    showLOG($info);
+
+                } else {
+                    echo 'nots user';
+                }
             } else {
+
                 echo 'nots user';
             }
-
-        } else {
-            echo 'nots user';
         }
 
         die;
