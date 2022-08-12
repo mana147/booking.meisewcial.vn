2ceed1b5d862982edbd51b33666cd7fc8f7956f1
hieupt <hieuphamtrung@admicro.vn>
2022-08-12 17:07:39 +0700

    update

--- app/default/controllers/landing/Landing.php
+++ app/default/controllers/landing/Landing.php
@@ -13,9 +13,15 @@ class Landing extends MY_Controller {
 
 		$data_view = [];
 
+		
+		$data_view['islogin'] = false;
+		
 		if ($this->_isLogin()) {
+
 			$data_view['info_user_email'] = $_SESSION['user_email'];
 			$data_view['info_user_nicename'] = $_SESSION['user_nicename'];
+
+			$data_view['islogin'] = true;
         }
 
         $this->load->view($this->_template_f . 'landing/landing_view.php', $data_view);
--- app/default/views/booking/landing/landing_view.php
+++ app/default/views/booking/landing/landing_view.php
@@ -121,67 +121,68 @@
         </div>
     </header>
 
+    <?php if ($islogin) { ?>
+        <!-- Calendar full -->
+        <div id='top' style="display: none;">
 
-    <!-- Calendar full -->
+            <div class='left'>
 
-    <div id='top' style="display: none;">
+                <div id='theme-system-selector' class='selector'>
+                    Theme System:
+                    <select>
+                        <option value='bootstrap5' selected>Bootstrap 5</option>
+                        <option value='bootstrap'>Bootstrap 4</option>
+                        <option value='standard'>unthemed</option>
+                    </select>
+                </div>
 
-        <div class='left'>
+                <div data-theme-system="bootstrap,bootstrap5" class='selector' style='display:none'>
+                    Theme Name:
+                    <select>
+                        <option value='' selected>Default</option>
+                        <option value='cerulean'>Cerulean</option>
+                        <option value='cosmo'>Cosmo</option>
+                        <option value='cyborg'>Cyborg</option>
+                        <option value='darkly'>Darkly</option>
+                        <option value='flatly'>Flatly</option>
+                        <option value='journal'>Journal</option>
+                        <option value='litera'>Litera</option>
+                        <option value='lumen'>Lumen</option>
+                        <option value='lux'>Lux</option>
+                        <option value='materia'>Materia</option>
+                        <option value='minty'>Minty</option>
+                        <option value='pulse'>Pulse</option>
+                        <option value='sandstone'>Sandstone</option>
+                        <option value='simplex'>Simplex</option>
+                        <option value='sketchy'>Sketchy</option>
+                        <option value='slate'>Slate</option>
+                        <option value='solar'>Solar</option>
+                        <option value='spacelab'>Spacelab</option>
+                        <option value='superhero'>Superhero</option>
+                        <option value='united'>United</option>
+                        <option value='yeti'>Yeti</option>
+                    </select>
+                </div>
 
-            <div id='theme-system-selector' class='selector'>
-                Theme System:
-                <select>
-                    <option value='bootstrap5' selected>Bootstrap 5</option>
-                    <option value='bootstrap'>Bootstrap 4</option>
-                    <option value='standard'>unthemed</option>
-                </select>
+                <span id='loading' style='display:none'>loading theme...</span>
+
             </div>
 
-            <div data-theme-system="bootstrap,bootstrap5" class='selector' style='display:none'>
-                Theme Name:
-                <select>
-                    <option value='' selected>Default</option>
-                    <option value='cerulean'>Cerulean</option>
-                    <option value='cosmo'>Cosmo</option>
-                    <option value='cyborg'>Cyborg</option>
-                    <option value='darkly'>Darkly</option>
-                    <option value='flatly'>Flatly</option>
-                    <option value='journal'>Journal</option>
-                    <option value='litera'>Litera</option>
-                    <option value='lumen'>Lumen</option>
-                    <option value='lux'>Lux</option>
-                    <option value='materia'>Materia</option>
-                    <option value='minty'>Minty</option>
-                    <option value='pulse'>Pulse</option>
-                    <option value='sandstone'>Sandstone</option>
-                    <option value='simplex'>Simplex</option>
-                    <option value='sketchy'>Sketchy</option>
-                    <option value='slate'>Slate</option>
-                    <option value='solar'>Solar</option>
-                    <option value='spacelab'>Spacelab</option>
-                    <option value='superhero'>Superhero</option>
-                    <option value='united'>United</option>
-                    <option value='yeti'>Yeti</option>
-                </select>
+            <div class='right'>
+                <span class='credits' data-credit-id='bootstrap-standard' style='display:none'>
+                    <a href='https://getbootstrap.com/docs/3.3/' target='_blank'>Theme by Bootstrap</a>
+                </span>
+                <span class='credits' data-credit-id='bootstrap-custom' style='display:none'>
+                    <a href='https://bootswatch.com/' target='_blank'>Theme by Bootswatch</a>
+                </span>
             </div>
 
-            <span id='loading' style='display:none'>loading theme...</span>
-
+            <div class='clear'></div>
         </div>
 
-        <div class='right'>
-            <span class='credits' data-credit-id='bootstrap-standard' style='display:none'>
-                <a href='https://getbootstrap.com/docs/3.3/' target='_blank'>Theme by Bootstrap</a>
-            </span>
-            <span class='credits' data-credit-id='bootstrap-custom' style='display:none'>
-                <a href='https://bootswatch.com/' target='_blank'>Theme by Bootswatch</a>
-            </span>
-        </div>
+        <div id='calendar'></div>
 
-        <div class='clear'></div>
-    </div>
-
-    <div id='calendar'></div>
+    <?php } ?>
 
     <!-- Team-->
     <section class="page-section bg-light" id="team">
@@ -250,98 +251,100 @@
     <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
     <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
 
+    <?php if ($islogin) { ?>
+        <!-- script full calendar -->
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                var calendarEl = document.getElementById('calendar');
+                var calendar;
 
-    <!-- script full calendar -->
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var calendarEl = document.getElementById('calendar');
-            var calendar;
+                initThemeChooser({
 
-            initThemeChooser({
+                    init: function(themeSystem) {
+                        calendar = new FullCalendar.Calendar(calendarEl, {
+                            themeSystem: themeSystem,
+                            headerToolbar: {
+                                left: 'prev,next today',
+                                center: 'title',
+                                right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+                            },
+                            initialDate: '2020-09-12',
+                            weekNumbers: true,
+                            navLinks: true, // can click day/week names to navigate views
+                            editable: true,
+                            selectable: true,
+                            nowIndicator: true,
+                            dayMaxEvents: true, // allow "more" link when too many events
+                            // showNonCurrentDates: false,
+                            events: [{
+                                    title: 'All Day Event',
+                                    start: '2020-09-01'
+                                },
+                                {
+                                    title: 'Long Event',
+                                    start: '2020-09-07',
+                                    end: '2020-09-10'
+                                },
+                                {
+                                    groupId: 999,
+                                    title: 'Repeating Event',
+                                    start: '2020-09-09T16:00:00'
+                                },
+                                {
+                                    groupId: 999,
+                                    title: 'Repeating Event',
+                                    start: '2020-09-16T16:00:00'
+                                },
+                                {
+                                    title: 'Conference',
+                                    start: '2020-09-11',
+                                    end: '2020-09-13'
+                                },
+                                {
+                                    title: 'Meeting',
+                                    start: '2020-09-12T10:30:00',
+                                    end: '2020-09-12T12:30:00'
+                                },
+                                {
+                                    title: 'Lunch',
+                                    start: '2020-09-12T12:00:00'
+                                },
+                                {
+                                    title: 'Meeting',
+                                    start: '2020-09-12T14:30:00'
+                                },
+                                {
+                                    title: 'Happy Hour',
+                                    start: '2020-09-12T17:30:00'
+                                },
+                                {
+                                    title: 'Dinner',
+                                    start: '2020-09-12T20:00:00'
+                                },
+                                {
+                                    title: 'Birthday Party',
+                                    start: '2020-09-13T07:00:00'
+                                },
+                                {
+                                    title: 'Click for Google',
+                                    url: 'http://google.com/',
+                                    start: '2020-09-28'
+                                }
+                            ]
+                        });
+                        calendar.render();
+                    },
 
-                init: function(themeSystem) {
-                    calendar = new FullCalendar.Calendar(calendarEl, {
-                        themeSystem: themeSystem,
-                        headerToolbar: {
-                            left: 'prev,next today',
-                            center: 'title',
-                            right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-                        },
-                        initialDate: '2020-09-12',
-                        weekNumbers: true,
-                        navLinks: true, // can click day/week names to navigate views
-                        editable: true,
-                        selectable: true,
-                        nowIndicator: true,
-                        dayMaxEvents: true, // allow "more" link when too many events
-                        // showNonCurrentDates: false,
-                        events: [{
-                                title: 'All Day Event',
-                                start: '2020-09-01'
-                            },
-                            {
-                                title: 'Long Event',
-                                start: '2020-09-07',
-                                end: '2020-09-10'
-                            },
-                            {
-                                groupId: 999,
-                                title: 'Repeating Event',
-                                start: '2020-09-09T16:00:00'
-                            },
-                            {
-                                groupId: 999,
-                                title: 'Repeating Event',
-                                start: '2020-09-16T16:00:00'
-                            },
-                            {
-                                title: 'Conference',
-                                start: '2020-09-11',
-                                end: '2020-09-13'
-                            },
-                            {
-                                title: 'Meeting',
-                                start: '2020-09-12T10:30:00',
-                                end: '2020-09-12T12:30:00'
-                            },
-                            {
-                                title: 'Lunch',
-                                start: '2020-09-12T12:00:00'
-                            },
-                            {
-                                title: 'Meeting',
-                                start: '2020-09-12T14:30:00'
-                            },
-                            {
-                                title: 'Happy Hour',
-                                start: '2020-09-12T17:30:00'
-                            },
-                            {
-                                title: 'Dinner',
-                                start: '2020-09-12T20:00:00'
-                            },
-                            {
-                                title: 'Birthday Party',
-                                start: '2020-09-13T07:00:00'
-                            },
-                            {
-                                title: 'Click for Google',
-                                url: 'http://google.com/',
-                                start: '2020-09-28'
-                            }
-                        ]
-                    });
-                    calendar.render();
-                },
+                    change: function(themeSystem) {
+                        calendar.setOption('themeSystem', themeSystem);
+                    }
 
-                change: function(themeSystem) {
-                    calendar.setOption('themeSystem', themeSystem);
-                }
+                });
 
             });
+        </script>
+    <?php } ?>
 
-        });
-    </script>
 </body>
 
 </html>
