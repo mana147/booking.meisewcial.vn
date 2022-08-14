08ef59256a40bba00dcf3695c9dce6fded66f848
hieupt <hieuphamtrung@admicro.vn>
2022-08-14 17:45:15 +0700

    update

--- app/default/views/booking/dashboard/dashboard_view.php
+++ app/default/views/booking/dashboard/dashboard_view.php
@@ -1,5 +1,10 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
+<!-- calendar -->
+<link href='fullcalendar-scheduler/lib/main.css' rel='stylesheet' />
+<script src='fullcalendar-scheduler/lib/main.js'></script>
+
+
 <body id="page-top">
 
     <!-- Page Wrapper -->
@@ -24,7 +29,8 @@
                 <a class="nav-link" href="https://booking.meisewcial.vn/dashboard">
                     <!-- <i class="fas fa-fw fa-tachometer-alt"></i> -->
                     <i class="fas fa-fw fa-table"></i>
-                    <span>Calendar Booking</span></a>
+                    <span>Calendar Booking</span>
+                </a>
             </li>
 
             <!-- Divider -->
@@ -146,7 +152,7 @@
                     <ul class="navbar-nav ml-auto">
 
                         <!-- Nav Item - Alerts -->
-                        <li style="display: none;"  class="nav-item dropdown no-arrow mx-1">
+                        <li style="display: none;" class="nav-item dropdown no-arrow mx-1">
                             <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-bell fa-fw"></i>
                                 <!-- Counter - Alerts -->
@@ -195,7 +201,7 @@
                         </li>
 
                         <!-- Nav Item - Messages -->
-                        <li style="display: none;"  class="nav-item dropdown no-arrow mx-1">
+                        <li style="display: none;" class="nav-item dropdown no-arrow mx-1">
                             <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <i class="fas fa-envelope fa-fw"></i>
                                 <!-- Counter - Messages -->
@@ -297,10 +303,152 @@
                         <h1 class="h3 mb-0 text-gray-800">Booking</h1>
                     </div>
 
-                    <!-- Content Row -->
-                    <div class="row">
+                    <!-- Calendar full -->
+                    <style>
+                        #calendar-container {
+                            position: fixed;
+                            top: 0;
+                            left: 0;
+                            right: 0;
+                            bottom: 0;
+                        }
 
+                        .fc-header-toolbar {
+                            padding-top: 1em;
+                            padding-left: 1em;
+                            padding-right: 1em;
+                        }
+                    </style>
+
+                    <div id='calendar-container'>
+                        <div id='calendar'></div>
                     </div>
+
+                    <script>
+                        document.addEventListener('DOMContentLoaded', function() {
+                            var calendarEl = document.getElementById('calendar');
+
+                            var calendar = new FullCalendar.Calendar(calendarEl, {
+                                height: '100%',
+                                aspectRatio: 1.8,
+                                editable: true, // enable draggable events
+                                now: '2020-09-07',
+                                scrollTime: '00:00', // undo default 6am scrollTime
+                                headerToolbar: {
+                                    left: 'today prev,next',
+                                    center: 'title',
+                                    right: 'resourceTimelineDay,resourceTimelineThreeDays,timeGridWeek,dayGridMonth,listWeek'
+                                },
+                                initialView: 'resourceTimelineDay',
+                                views: {
+                                    resourceTimelineThreeDays: {
+                                        type: 'resourceTimeline',
+                                        duration: {
+                                            days: 3
+                                        },
+                                        buttonText: '3 days'
+                                    }
+                                },
+                                expandRows: true,
+                                resourceAreaHeaderContent: 'Rooms',
+                                resources: [{
+                                        id: 'a',
+                                        title: 'Auditorium A'
+                                    },
+                                    {
+                                        id: 'b',
+                                        title: 'Auditorium B',
+                                        eventColor: 'green'
+                                    },
+                                    {
+                                        id: 'c',
+                                        title: 'Auditorium C',
+                                        eventColor: 'orange'
+                                    },
+                                    {
+                                        id: 'd',
+                                        title: 'Auditorium D',
+                                        children: [{
+                                                id: 'd1',
+                                                title: 'Room D1'
+                                            },
+                                            {
+                                                id: 'd2',
+                                                title: 'Room D2'
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        id: 'e',
+                                        title: 'Auditorium E'
+                                    },
+                                    {
+                                        id: 'f',
+                                        title: 'Auditorium F',
+                                        eventColor: 'red'
+                                    },
+                                    // { id: 'g', title: 'Auditorium G' },
+                                    // { id: 'h', title: 'Auditorium H' },
+                                    // { id: 'i', title: 'Auditorium I' },
+                                    // { id: 'j', title: 'Auditorium J' },
+                                    // { id: 'k', title: 'Auditorium K' },
+                                    // { id: 'l', title: 'Auditorium L' },
+                                    // { id: 'm', title: 'Auditorium M' },
+                                    // { id: 'n', title: 'Auditorium N' },
+                                    // { id: 'o', title: 'Auditorium O' },
+                                    // { id: 'p', title: 'Auditorium P' },
+                                    // { id: 'q', title: 'Auditorium Q' },
+                                    // { id: 'r', title: 'Auditorium R' },
+                                    // { id: 's', title: 'Auditorium S' },
+                                    // { id: 't', title: 'Auditorium T' },
+                                    // { id: 'u', title: 'Auditorium U' },
+                                    // { id: 'v', title: 'Auditorium V' },
+                                    // { id: 'w', title: 'Auditorium W' },
+                                    // { id: 'x', title: 'Auditorium X' },
+                                    // { id: 'y', title: 'Auditorium Y' },
+                                    // { id: 'z', title: 'Auditorium Z' }
+                                ],
+                                events: [{
+                                        id: '1',
+                                        resourceId: 'b',
+                                        start: '2020-09-07T02:00:00',
+                                        end: '2020-09-07T07:00:00',
+                                        title: 'event 1'
+                                    },
+                                    {
+                                        id: '2',
+                                        resourceId: 'c',
+                                        start: '2020-09-07T05:00:00',
+                                        end: '2020-09-07T22:00:00',
+                                        title: 'event 2'
+                                    },
+                                    {
+                                        id: '3',
+                                        resourceId: 'd',
+                                        start: '2020-09-06',
+                                        end: '2020-09-08',
+                                        title: 'event 3'
+                                    },
+                                    {
+                                        id: '4',
+                                        resourceId: 'e',
+                                        start: '2020-09-07T03:00:00',
+                                        end: '2020-09-07T08:00:00',
+                                        title: 'event 4'
+                                    },
+                                    {
+                                        id: '5',
+                                        resourceId: 'f',
+                                        start: '2020-09-07T00:30:00',
+                                        end: '2020-09-07T02:30:00',
+                                        title: 'event 5'
+                                    }
+                                ]
+                            });
+
+                            calendar.render();
+                        });
+                    </script>
 
 
                 </div>
